f60dbd9e2215f185aa15054f99e7fa63762ea77b
SONG22 <ansongi527@gmail.com>
2022-01-06 22:16:26 +0900

    item 18

---
+++ src/main/java/effective_java/item18/InstrumentedSet.java
@@ -0,0 +1,19 @@
+package effective_java.item18;
+
+import java.util.Set;
+
+// wrapper class
+public class InstrumentedSet<E> extends ForwardingSet<E> {
+
+    private int addCount = 0;
+
+    public InstrumentedSet(Set<E> s) {
+        super(s);
+    }
+
+    @Override
+    public boolean add(E e) {
+        addCount++;
+        return super.add(e);
+    }
+}
